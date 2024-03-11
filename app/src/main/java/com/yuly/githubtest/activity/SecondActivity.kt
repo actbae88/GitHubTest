bb7599e480016af88176b23b04bbae8c94f67571
actbae88 <actbae88@gmail.com>
2024-03-11 13:42:30 +0900

    SecondActivity viewbind했다..

--- app/src/main/java/com/yuly/githubtest/activity/SecondActivity.kt
+++ app/src/main/java/com/yuly/githubtest/activity/SecondActivity.kt
@@ -3,10 +3,15 @@ package com.yuly.githubtest.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.yuly.githubtest.R
+import com.yuly.githubtest.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivitySecondBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
