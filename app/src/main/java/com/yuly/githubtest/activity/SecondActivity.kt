4804c9493adaf17888312abd953237fd57bc1e99
actbae88 <actbae88@gmail.com>
2024-03-11 13:40:44 +0900

    add SecondActivity and image file

--- app/src/main/java/com/yuly/githubtest/activity/SecondActivity.kt
+++ app/src/main/java/com/yuly/githubtest/activity/SecondActivity.kt
@@ -1,11 +1,12 @@
-package com.yuly.githubtest
+package com.yuly.githubtest.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.yuly.githubtest.R
 
-class MainActivity : AppCompatActivity() {
+class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_second)
     }
 }
