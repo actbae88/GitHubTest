953eb87a7e14f51a7167e40e6b5a52591b1a9b5f
actbae88 <actbae88@gmail.com>
2024-03-11 14:04:15 +0900

    binding.tv.setonclicklistener -->toast

--- app/src/main/java/com/yuly/githubtest/activity/MainActivity.kt
+++ app/src/main/java/com/yuly/githubtest/activity/MainActivity.kt
@@ -2,6 +2,7 @@ package com.yuly.githubtest.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.yuly.githubtest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +13,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        
+        binding.tv.setOnClickListener { Toast.makeText(this, "click textview", Toast.LENGTH_SHORT).show() }
     }
 }
